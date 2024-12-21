649f480a955d98ccb736f75cb366d7ebab1af96b
Omar-Obide <3omar.obide@gmail.com>
2024-12-21 16:05:41 +0200

    api key

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -5,7 +5,7 @@ import axios, { AxiosRequestConfig } from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://ai-weather-by-meteosource.p.rapidapi.com",
   headers: {
-    "x-rapidapi-key": "",
+    "x-rapidapi-key": "2b3ffcb2b9mshad614c7d2c5cc80p1dfdadjsn35ea857209b9",
     "X-RapidAPI-Host": "ai-weather-by-meteosource.p.rapidapi.com",
   },
 });
