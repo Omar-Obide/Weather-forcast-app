12fe7c6f1b6ff202bdf22cac82d47f8bfa393348
Omar-Obide <3omar.obide@gmail.com>
2024-12-21 16:07:22 +0200

    api key

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,11 +1,12 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 //2b3ffcb2b9mshad614c7d2c5cc80p1dfdadjsn35ea857209b9
+//0e5b62d35amshec7ffd06706a706p159c71jsn12667642a348
 
 const axiosInstance = axios.create({
   baseURL: "https://ai-weather-by-meteosource.p.rapidapi.com",
   headers: {
-    "x-rapidapi-key": "2b3ffcb2b9mshad614c7d2c5cc80p1dfdadjsn35ea857209b9",
+    "x-rapidapi-key": "0e5b62d35amshec7ffd06706a706p159c71jsn12667642a348",
     "X-RapidAPI-Host": "ai-weather-by-meteosource.p.rapidapi.com",
   },
 });
